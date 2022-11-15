7ab913997a0a8b11220a26b817474060483493c6
Daniel Williams <danielpaulwill@gmail.com>
2022-11-14 21:40:40 -0500

    finalized files

--- src/Header.js
+++ src/Header.js
@@ -30,7 +30,7 @@ function Header({ handleSelect, handleClick }) {
             </div>
         </div>
         <div className="row info">
-          <h4 id="infoText">A normal cup of coffee has 95mg of caffeine</h4>
+          <p className="h5" id="infoText">A normal cup of coffee has 95mg of caffeine</p>
           <br></br>
           {/* <img 
             src="https://lh3.googleusercontent.com/PaToWpYqlj3RZv46tzYiLm7FvsOxQhjno42QhHSQyqihnVeNGH_mcLCw-iKgf8tTCkmR8jxvsobo6ZUvGQB9f9E_iGfDWSKwnJb_YgD1kGc8uX5l0Qs9zX9Jebdx522haZhp89fyL7oKdLnP4ThQVWLSufIvtoDBG16GdGkHsOUTXPZCT5RRtUP6LpGk0i7jYaZLKXCjrxIRJ8J_QSwt5FqWtYEV6YmfRPcZovN2MGPZGAJbgUb3kiO3cKAqOFThHnzexD3_7d1OLQxqB8ptUtYOMEZ_D_J9miLtFM5IZaHcpDCzvXHOrS-NmTFyek-xdRxpGBUQjC6PmjkCzOGZyYqUZh4KhssjCODzzjAfBSfMevuEtx6c_WGYkrM_AMZDwoB3YtWcKcVM6GLWiwhZ8P2ej7OGlB7d9E3FMTuL9x6qQCyc0jTKe80IJukJQKusB-hhZtmYXsa0JSsOyjpS1vv_LLa0LzLtZmcQ45KkbASCQs2eH9UeuKEUS8Au1KF-0UqpQrGuu_TNSoYXj4q1t7oLkqvVF5iqgmOlnmwxBbuWUSPfkm7MLTA6Of_GserHTQPgC-4wI7f9y2Uy2f6tkOTHVcdwKBbaQRZPwHSt3NJHJaI3ozam5IIN8sjWmMKZOdG_k63Q-KVFReIdFOmlGjT-kSi8Gmv7HKpoKhWo4Psf2nqU8XekeH5B7XVbmHXHG7_REuI8koZGXq0oJ1E_H9pS=w171-h100-no?authuser=0"
